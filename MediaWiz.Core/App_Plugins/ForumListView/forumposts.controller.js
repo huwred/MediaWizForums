b78ae888d739a29f6260f581a3d62a76c9627978
huwred <huw@forum.snitz.com>
2023-08-15 08:48:51 +0100

    Custom Listview for replies added

---
+++ MediaWiz.Core/App_Plugins/ForumListView/forumposts.controller.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    "use strict";
+
+    function ForumPostsController($scope, listViewHelper, $location, entityResource) {
+
+        var vm = this;
+
+        vm.selectItem = selectItem;
+        vm.clickItem = clickItem;
+
+
+        // Init the controller
+        function activate() {
+
+            // Load parent Topic for each item
+            angular.forEach($scope.items, function (item) {
+                entityResource.getById(item.parentId,"Document")
+                      .then(function(data) {
+                          item.parentTopic = data.name;
+                      });
+            });
+            
+        }
+
+        // Item select handler
+        function selectItem(selectedItem, $event, index) {
+
+            // use the list view helper to select the item
+            listViewHelper.selectHandler(selectedItem, index, $scope.items, $scope.selection, $event);
+            $event.stopPropagation();
+
+        }
+
+        // Item click handler
+        function clickItem(id) {
+
+            // change path to edit item
+            $location.path($scope.entityType + '/' + $scope.entityType + '/edit/' + id);
+
+        }
+
+        activate();
+
+    }
+
+    angular.module("umbraco").controller("My.ListView.Layout.ForumPostsController", ForumPostsController);
+
+})();
---
+++ MediaWiz.Core/Events/ForumPostDeletedHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Examine;
+using MediaWiz.Forums.Indexing;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Infrastructure.Examine;
+
+namespace MediaWiz.Forums.Events;
+
+public class ForumPostDeletedHandler : INotificationHandler<ContentMovedToRecycleBinNotification>
+{
+    private readonly ILogger<ForumPostDeletedHandler> _logger;
+    private readonly IExamineManager _examineManager;
+    private readonly IIndexRebuilder _indexRebuilder;
+
+    public ForumPostDeletedHandler(IIndexRebuilder indexRebuilder,IExamineManager examineManager,ILogger<ForumPostDeletedHandler> logger)
+    {
+        _examineManager = examineManager;
+        _logger = logger;
+        _indexRebuilder = indexRebuilder;
+    }
+
+    public void Handle(ContentMovedToRecycleBinNotification notification)
+    {
+
+        bool rebuild = false;
+        foreach (var node in notification.MoveInfoCollection)
+        {
+            if (node.Entity.ContentType.Alias.Equals("forumPost"))
+            {
+                rebuild = true;
+            }
+        }
+
+        if (rebuild)
+        {
+            _indexRebuilder.RebuildIndex("ForumIndex", null, true);
+        }
+    }
+}
